d6440d9dc36cbd5dd5fec38e3e979eb48b170e5c
Eduardo Neto <eduardojoselinoneto@gmail.com>
2025-07-03 19:42:03 -0300

    Resolvendo o desafio utilizando classe abstract e sobrescrevendo um método

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", "32GB");
+nokia.Ligar();
+
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", "128GB");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Console.WriteLine();
+Console.WriteLine("Teste concluído com sucesso!");      
